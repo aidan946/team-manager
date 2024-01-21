4c4f82903ece9c251506b1e878bb2032b067e8b4
Aidan S <aidanstevens90@gmail.com>
2024-01-21 13:35:02 +1100

    init

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  root 'login#index'
+
+  post 'login' => 'login#login'
+  post 'sign_up' => 'login#sign_up'
+
+  resources :users
+end
