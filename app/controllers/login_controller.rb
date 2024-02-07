16290a5bb49ed37714bd5660517a5b13d6dc9061
Aidan <aidanstevens90@gmail.com>
2024-02-07 19:17:09 +1100

    Add login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,17 @@
 class LoginController < ApplicationController
+  layout 'login'
+
   def index; end
 
-  def login; end
+  def login
+    user = User.where(email: params[:email]).take
+    if user.valid_password?(params[:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      redirect_to root_path
+    end
+  end
 
   def sign_up; end
 end
