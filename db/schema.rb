e13aa2c98f09960094bffc460b6aeeb2fb12de11
Aidan <aidanstevens90@gmail.com>
2024-02-10 16:52:06 +1100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_100705) do
-  create_table "teams", charset: "utf8mb4", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_075736) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
     t.string "status"
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_100705) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_100705) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
