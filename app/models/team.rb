80128724bb885e5d9d511a07cd7664b236aac00c
Aidan <aidanstevens90@gmail.com>
2024-06-27 19:42:26 +1000

    Format

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,8 @@ class Team < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   has_many :members, dependent: :destroy
   has_many :users, through: :members
+  has_many :posts, dependent: :destroy
+
   validates :name, presence: true
   validates :owner_id, presence: true
 end
